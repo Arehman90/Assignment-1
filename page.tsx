aee9e2d9a814675af4f4f7bc8f42559ebd4e5eae
Abdul Rehman <155440987+Arehman90@users.noreply.github.com>
2024-11-23 16:05:30 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
+import { error } from "console";
 
-
-
-
-export default function Home () {
+export default function Contactus() {
+     throw new Error
   return (
-    <div className="p-20 w-10/19 text-lg">
-      <h1 className="text-center  text-3xl font-bold" >Hello...!</h1>
-      <br /><br />
-      <p>Hi! My name is Abdul rehman, 
-      currently exploring the exciting world of Artificial Intelligence and Web 3.0. 
-      As part of my journey at the Governor House, I’m learning to build next-gen applications 
-      that push the boundaries of what’s possible.
-    <br />
-    <br />
-      From developing dynamic web projects to exploring cutting-edge AI technologies, 
-      I’m diving into the world of tech with a focus on hands-on learning and innovation. 
-      Join me as I grow my skills, share my projects, and discover the future of the web.</p>
+    <div className="p-8">
+      <h1 className="font-mono font-bold text-4xl">
+        <i>
+          
+          <u> Contact-ME! </u>
+        </i>
+      </h1>
+      <br />
+      <p className="text-lg">
+        1)<i> ar5507198@gmail.com </i>
+      </p>
+      <p className="text-lg">
+        {" "}
+        2)<i> 03197952903.</i>
+      </p>
     </div>
-  )
+  );
 }
